920c7d9cb83cc6711c92ed89d00b9c48d4756fb7
Artem Startsev <smershik88@yandex.ru>
2026-09-18 22:26:03 +0300

    hw_1 done

---
+++ hw_1/A1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+
+    /*
+    Let's
+      go
+        to walk
+    */
+
+    printf("Let's\n");
+    printf("  go\n");
+    printf("    to walk\n");
+
+    return EXIT_SUCCESS;
+}
---
+++ hw_1/A2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+
+    /*
+     *
+    ***
+   *****
+  *******
+   HHOHH
+   ZZZZZ
+    */
+
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HHOHH\n");
+    printf(" ZZZZZ\n");
+
+    return EXIT_SUCCESS;
+}
